a806cde8a108a35fff9ce124d804cf9e4042951f
wuziming <1445031423@qq.com>
2023-03-29 01:32:45 +0800

    WIP: 动效

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Logo from './components/Logo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          吴梓铭
-        </a>
-      </header>
+      <Logo></Logo>
+
     </div>
   );
 }
---
+++ src/components/Logo/index.js
@@ -0,0 +1,30 @@
+import "./index.css"
+
+export default function Logo() {
+
+  const showModal = () => {
+    const fullBg = document.querySelector('.full-bg')
+    fullBg.classList.add('left-to-right')
+    fullBg.classList.add('radius')
+    setTimeout(() => {
+        fullBg.classList.remove('radius')
+    }, 500)
+    setTimeout(() => {
+        fullBg.classList.remove('left-to-right')
+    }, 1200)
+  } 
+
+  return (
+    <>
+        <div className="box">
+            <div className="header">
+                <button id="btn" onClick={showModal}>幕布</button>
+                <div className="clicking">header</div>
+            </div>
+            <div className="main">main</div>
+            <div className="bottom">bottom</div>
+        </div>
+        <div className="full-bg"></div>
+    </>
+  );
+}
