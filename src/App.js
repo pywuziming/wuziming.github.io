34ad2c2db0ead790205ae263f65b705f9ea65fae
jasperzmwu <jasperzmwu@wesure.cn>
2023-03-30 11:44:41 +0800

    WIP： PC端已完成

--- src/App.js
+++ src/App.js
@@ -1,40 +1,61 @@
-// import logo from './logo.svg';
 import './App.css';
-// import Logo from './components/Logo';
 import logo from './static/logo.png'
+import wxImg from './static/wxImg.png'
 
 function App() {
+
+  const showPageUp = () => {
+    const fullBg = document.querySelector('.full-bg')
+    fullBg.classList.add('left-to-right')
+    fullBg.classList.add('radius')
+    setTimeout(() => {
+        fullBg.classList.remove('radius')
+    }, 500)
+    setTimeout(() => {
+        fullBg.classList.remove('left-to-right')
+        fullBg.classList.add('no-display')
+    }, 1000)
+  }
+
+  const handleMove = () => {
+    const fullBg = document.querySelector('.wx')
+    fullBg.classList.add('wx-move')
+  };
+
   return (
-    <div className="App">
-      {/* <Logo></Logo> */}
-      <header className='header-container'>
-        <div className='content'>
-          <div className='left-content'>
-            <img alt='' src={logo} className='img'/>
+    <>
+      <div className="App">
+        <header className='header-container'>
+          <div className='content'>
+            <div className='left-content' onClick={showPageUp}>
+              <img alt='' src={logo} className='img'/>
+            </div>
+            <div className='right-content'>
+              <div className='txt-btn click-btn'>案例</div>
+              <div className='txt-btn'>关于我们</div>
+              <div className='txt-btn'>EN</div>
+            </div>
           </div>
-          <div className='right-content'>
-            <div className='txt-btn click-btn'>案例</div>
-            <div className='txt-btn'>关于我们</div>
-            <div className='txt-btn'>EN</div>
+        </header>
+        <main className='main-container'>
+          <div className='title'>灵境至维</div>
+          <div className='txt-ENglish'>YOU CAN EXPECT MORE</div>
+          <div className='txt-desc'>
+            <div>社交网络传播在内的多样化功能，这些虚拟空间可以相互连通，构建连接的虚拟社区和世界。</div>
+            <div>灵境至维是为品牌和艺术活动打造连接和开放的虚拟空间的平台。</div>
+            <div>每一个虚拟空间都可以呈现包含开放世界、在线直播、高保真3D体验、互动视频、游戏化人物形象和社交网络传播在内的多样化功能，这些虚拟空间可以相互连通，构建连接的虚拟社区和世界。</div>
           </div>
-        </div>
-      </header>
-      <main className='main-container'>
-        <div className='title'>灵境至维</div>
-        <div className='txt-ENglish'>YOU CAN EXPECT MORE</div>
-        <div className='txt-desc'>
-          <div>社交网络传播在内的多样化功能，这些虚拟空间可以相互连通，构建连接的虚拟社区和世界。</div>
-          <div>灵境至维是为品牌和艺术活动打造连接和开放的虚拟空间的平台。</div>
-          <div>每一个虚拟空间都可以呈现包含开放世界、在线直播、高保真3D体验、互动视频、游戏化人物形象和社交网络传播在内的多样化功能，这些虚拟空间可以相互连通，构建连接的虚拟社区和世界。</div>
-        </div>
-        
-        <div className='btn-container'>
-          <div>联系我们</div>
-          <div>关注我们</div>
-        </div>
-      </main>
-      <footer className='header-container'>底部</footer>
-    </div>
+          <div className='btn-container'>
+            <div className='btn' onClick={handleMove}>联系我们</div>
+            <div className='btn'>关注我们</div>
+          </div>
+        </main>
+      </div>
+      <div className="full-bg"></div>
+      <div className="wx" onClick={handleMove}>
+        <img alt='' src={wxImg} className="wx-img"/>
+      </div>
+    </>
   );
 }
 
